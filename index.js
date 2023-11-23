0d5be8ecc16786321cd6257c2921b2aa61b133b8
joacocrespi <131682675+joacocrespi@users.noreply.github.com>
2023-11-23 12:21:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -50,3 +50,12 @@ function makeSound(key) {
       break;
   }
 }
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
+
